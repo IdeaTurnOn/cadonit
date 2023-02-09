6974a8f30704dd9c4dde50c77e002742fc2e8f90
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-02-09 11:35:29 +0530

    header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'f-nato': ['Noto Sans', 'sans-serif'],
+        'cadpara': ['Gentium Book Plus', 'serif'],
+        
+    },
+    },
   },
   plugins: [],
 }
